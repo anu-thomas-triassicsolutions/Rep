5e78c2e7c7e7c05fbe39bab9b8d1acfad7ec54ff
anu-thomas-triassicsolutions <107909020+anu-thomas-triassicsolutions@users.noreply.github.com>
2022-07-04 10:58:26 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,21 +1,23 @@
 # Print simple Hello World
-class Intro():
+class Intro:
     def intro(self):
         # Take the object and print Hallo World
         print("Hallo world")
 
-object = Intro() # creation of an object variable
-object.intro() # Accessing class methods
+
+object1 = Intro()  # creation of an object variable
+object1.intro()  # Accessing class methods
 
 
 # Simple addition
-class Operator():
-    def __init__(self,a,b): # passing the object and arguments
-        self.a = a
-        self.b = b
+class Operator:
+    def __init__(self,num1,num2):  # passing the object and arguments
+        self.num1 = num1
+        self.num2 = num2
 
     def sum(self):
-        print(self.a+self.b) # print the sum of two values
+        print(self.num1+self.num2)  # print the sum of two values
+
 
 ans = Operator(1,3)  # creation of an object variable
 ans.sum()  # Accessing class methods
